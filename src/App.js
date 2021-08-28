254c3ac943d8052358d247f643ae1902b898ad25
unknown <aayushanand@gmail.com>
2021-08-28 10:19:57 +0530

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+  <div>
+    <div className="container">
+    
+    <ul>
+    <li>contact</li>
+    <li>service </li>
+    <li>about</li>
+      <li>Home</li>
+     
+      
+     
+    </ul>
     </div>
+    <h1>Lets get started!</h1>
+    
+    <h2>Hello React</h2>
+    <h5>Myself Aayush anand a sophmore in vit-ap university, interested in learning and developing new ideas and projects. </h5>
+    
+  </div>
   );
-}
 
+}
 export default App;
