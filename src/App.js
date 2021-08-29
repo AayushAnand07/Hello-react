46dd6d64d0d1782add718d4a62410bb393b38ea8
unknown <aayushanand@gmail.com>
2021-08-29 23:23:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import './App.css';
+import DnsRoundedIcon from '@material-ui/icons/DnsRounded';
+
 function App(){
   return(
-  <div>
-    <div className="container">
-    
-    <ul>
-    <li>contact</li>
-    <li>service </li>
-    <li>about</li>
-      <li>Home</li>
-     
+  <nav>
       
-     
-    </ul>
-    </div>
-    <h1>Lets get started!</h1>
-    
-    <h2>Hello React</h2>
-    <h5>Myself Aayush anand a sophmore in vit-ap university, interested in learning and developing new ideas and projects. </h5>
-    
-  </div>
-  );
+      <input type="checkbox" id="check"/>
+     <label for="check" class="checkbtn"> <DnsRoundedIcon></DnsRoundedIcon></label>
 
+    <label class="logo">NavBar</label>
+    <ul>
+      <li ><a class="active" href="/home">Home</a></li>
+      <li><a href="/home">About</a></li>
+      <li><a href="/home">Services</a></li>
+      <li><a href="/home">Contact</a></li>
+      <li><a href="/home">Gallery</a></li>
+    </ul>
+    <section>
+    <h2>Hello React</h2>
+    <h5>Myself Aayush anand a sophmore in vit-ap university,
+       interested in learning and developing new
+        ideas and projects. </h5>
+    </section>
+   
+    
+  </nav>
+  
+  );
+  
+ /* <h2>Hello React</h2>
+    <h5>Myself Aayush anand a sophmore in vit-ap university, interested in learning and developing new ideas and projects. </h5> */
 }
 export default App;
